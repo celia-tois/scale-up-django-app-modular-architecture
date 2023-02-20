46b4d5f9e9641d1d581f406590a44c8adb1c9e20
Célia Tois <celia.tois@gmail.com>
2023-02-20 19:50:24 +0100

    put profiles in its own app

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from lettings.views import lettings_index, letting
+from profiles.views import profiles_index, profile
 
 from . import views
 
@@ -8,7 +9,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', lettings_index, name='lettings_index'),
     path('lettings/<int:letting_id>/', letting, name='letting'),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
+    path('profiles/', profiles_index, name='profiles_index'),
+    path('profiles/<str:username>/', profile, name='profile'),
     path('admin/', admin.site.urls),
 ]
