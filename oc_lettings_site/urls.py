27b2f5160e16157736c9174403e435ba8f306068
Célia Tois <celia.tois@gmail.com>
2023-05-29 12:19:18 +0200

    install sentry and create sentry-debug/ endpoint

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
 
 urlpatterns = [
+    path('sentry-debug/', trigger_error),
     path('', include('home.urls')),
     path('lettings/', include('lettings.urls')),
     path('profiles/', include('profiles.urls')),
